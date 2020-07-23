b7c07f9c3806c3a630716c5e1c634f31df38b9fd
pqv199x <pqv199x@hotmail.com>
2020-07-23 13:57:24 +0700

    add tron

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const tomo = require('./tomo')
 const bitcoin = require('./btc')
+const tron = require('./tron')
 const RevealAddress = {
     tomo: (words, offset, hdPath) => {
         return tomo(words, offset, hdPath)
     },
     bitcoin: (words, offset, hdPath) => {
         return bitcoin(words, offset, hdPath)
+    },
+    tron: (words, offset, hdPath) => {
+        return tron(words, offset, hdPath)
     }
 }
 
---
+++ tron.js
@@ -0,0 +1,69 @@
+const bip39 = require('bip39')
+const bitcoinjs = require('bitcoinjs-lib')
+const HDKey = require('hdkey')
+const TronWeb = require('tronweb')
+const EC = require('elliptic').ec
+
+const string2byte = (str) => {
+    let byteArray = []
+    for (let j = 0; j < str.length; j++) {
+        byteArray.push(str.charCodeAt(j))
+    }
+
+    return byteArray
+}
+
+const getPubKeyFromPriKey = (priKeyBytes) => {
+    const ec = new EC('secp256k1');
+    const key = ec.keyFromPrivate(priKeyBytes, 'bytes');
+    const pubkey = key.getPublic();
+    const x = pubkey.x;
+    const y = pubkey.y;
+
+    let xHex = x.toString('hex');
+
+    while (xHex.length < 64) {
+        xHex = `0${xHex}`;
+    }
+
+    let yHex = y.toString('hex');
+
+    while (yHex.length < 64) {
+        yHex = `0${yHex}`;
+    }
+
+    return `04${xHex}${yHex}`;
+}
+
+async function revealAddress (words, offset, hdPath) {
+    try {
+        const seed = bip39.mnemonicToSeedSync(words)
+        const hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'))
+        const result = []
+        for (let i = 0; i < offset; i++) {
+            const childkey = hdkey.derive(`${hdPath}/${i}`)
+            const pri = childkey.privateKey.toString('hex')
+            const tronweb = new TronWeb(
+                'http://127.0.0.1:9090',
+                'http://127.0.0.1:9090',
+                'http://127.0.0.1:9090',
+                pri,
+            )
+
+            const pub = getPubKeyFromPriKey(tronweb.utils.code.hexStr2byteArray(pri))
+
+            result.push({
+                index: i,
+                address: tronweb.address.fromPrivateKey(pri),
+                publicKey: pub.toUpperCase(),
+                privateKey: pri.toUpperCase()
+            })
+        }
+        return result
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = revealAddress
